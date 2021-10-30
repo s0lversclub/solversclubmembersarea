a8cfd7bc09e20ff13b097d3c9a5be85a262c5bc0
Solvers Club <82651188+s0lversclub@users.noreply.github.com>
2021-10-30 22:01:06 +0200

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -11,21 +11,30 @@ router.get('/join', appController.getJoin);
 router.get('/login', appController.getLogin);
 router.get('/logout', userController.logout);
 router.get('/profile', userController.isLoggedIn, appController.getProfile);
-router.get('/forgot-password', appController.getForgetPassword);
+router.get('/forgot', appController.getForgetPassword);
+router.get('/activate', appController.getActivate);
 router.get('/login/resetlink/:token', appController.getResetPassword);
+router.get('/activate/activatelink/:token', appController.getConfirmActivate);
 router.get('/members', userController.isLoggedIn, appController.getMembersDirectory);
 router.get('/discovery', userController.isLoggedIn, appController.getDiscovery);
 router.get('/update', userController.isLoggedIn, appController.getMembersUpdate);
 router.get('/resources', userController.isLoggedIn, appController.getResources);
 router.get('/delete', userController.isLoggedIn, appController.getDelete);
+router.get('/thankyou', appController.getApplied);
+router.get('/tc', appController.getTC);
+router.get('/privacy', appController.getPrivacy);
 
 router.post(
   '/user/add',
   userController.addUser,
   userController.storePassword,
-  appController.getLogin
+  appController.getApplied
 );
-router.post('/user/auth', userController.authenticate);
+router.post(
+  '/user/auth',
+  userController.checkUser,
+  userController.authenticate,
+);
 router.post(
   '/user/forgot',
   userController.addToken,
@@ -36,6 +45,17 @@ router.post(
   userController.confirmToken,
   userController.storePassword,
   userController.sendConfirmResetEmail
+);
+router.post(
+  '/user/activate',
+  userController.addActivationToken,
+  userController.sendActivationEmail
+);
+router.post(
+  '/user/confirmactivate',
+  userController.confirmToken,
+  userController.storePassword,
+  userController.sendConfirmActivationEmail
 );
 
 module.exports = router;
